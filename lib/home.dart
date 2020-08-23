fa52b709053d162e1f9e78d2fe549cc8f36dace3
alimtegar <alimtegar4@gmail.com>
2020-08-23 13:01:19 +0700

    Create Search

--- lib/home.dart
+++ lib/home.dart
@@ -12,83 +12,155 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  
+  TextEditingController searchController = TextEditingController();
+  bool isSearching = false;
+  String query = '';
+
+  List<Map<String, dynamic>> list;
+  List<Map<String, dynamic>> filteredList;
+
+  @override
+  void initState() {
+    super.initState();
+
+    list = LearningList.learningList;
+  }
+
+  _HomeState() {
+    searchController.addListener(() {
+      if (searchController.text.isEmpty) {
+        setState(() {
+          isSearching = false;
+          query = '';
+        });
+      } else {
+        setState(() {
+          isSearching = true;
+          query = searchController.text;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.menu),
-            tooltip: 'Menu',
-            onPressed: () {},
-          )
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(65.0),
-          child: Theme(
-              data: Theme.of(context).copyWith(accentColor: Colors.white),
-              child: Container(
-                height: 50.0,
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(right: 15, bottom: 15, left: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: Icon(Icons.search),
-                      hintText: 'Type here to search item...',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.all(15)),
-                  style: TextStyle(
-                    fontSize: 14,
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.menu),
+              tooltip: 'Menu',
+              onPressed: () {},
+            )
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(65.0),
+            child: Theme(
+                data: Theme.of(context).copyWith(accentColor: Colors.white),
+                child: Container(
+                  height: 50.0,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(right: 15, bottom: 15, left: 15),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: Icon(Icons.search),
+                        hintText: 'Type here to search item...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: EdgeInsets.all(15)),
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                    //   textAlignVertical: TextAlignVertical.center,
+                    onChanged: null,
                   ),
-                  //   textAlignVertical: TextAlignVertical.center,
-                  onChanged: null,
-                ),
-              )),
-        ),
-      ),
-      body: ListView(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        shrinkWrap: true,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 4),
-                  child: Text(
-                    'List to Learn',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-                Text(
-                  'Choose subject below to learn it',
-                ),
-              ],
-            ),
+                )),
           ),
-          Container(
-              child: ListView.builder(
-				  shrinkWrap: true,
-				  itemCount: LearningList.learningList.length,
-                  itemBuilder: (BuildContext context, int index) =>
-                      LearningItem(
-						  id: LearningList.learningList[index]['id'],
-                        title: LearningList.learningList[index]['title'],
-						image: LearningList.learningList[index]['image'],
-						description: LearningList.learningList[index]['description'],
-                      ))),
-        ],
-      ),
+        ),
+        body: !isSearching ? _buildListView() : _performSearch());
+  }
+
+  Widget _buildListView() {
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4),
+                child: Text(
+                  'List to Learn',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+              Text(
+                'Choose subject below to learn it',
+              ),
+            ],
+          ),
+        ),
+        Container(
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) => LearningItem(
+                      id: list[index]['id'],
+                      title: list[index]['title'],
+                      image: list[index]['image'],
+                      description: list[index]['description'],
+                    ))),
+      ],
+    );
+  }
+
+  Widget _performSearch() {
+    filteredList = list.where((i) => i['title'].toLowerCase().contains(query.toLowerCase()) || i['content'].toLowerCase().contains(query.toLowerCase())).toList();
+
+    return _buildFilteredListView();
+  }
+
+  Widget _buildFilteredListView() {
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4),
+                child: Text(
+                  'List to Learn',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+              Text(
+                'Choose subject below to learn it',
+              ),
+            ],
+          ),
+        ),
+        Container(
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: filteredList.length,
+                itemBuilder: (BuildContext context, int index) => LearningItem(
+                      id: filteredList[index]['id'],
+                      title: filteredList[index]['title'],
+                      image: filteredList[index]['image'],
+                      description: filteredList[index]['description'],
+                    ))),
+      ],
     );
   }
 }
